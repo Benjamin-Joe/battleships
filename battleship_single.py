e9d8a5d75430ba30fcc48270e598a5077d56be20
Benjamin-Joe <benjaminjoe43@gmail.com>
2021-09-22 21:19:18 +0000

    Create base boards and ships

---
+++ battleship_single.py
@@ -0,0 +1,51 @@
+# Legend
+# X for placing ship and hitting ship
+# ' ' for availiable space
+# '-' for missed
+
+from random import randint
+
+hidden_board = [[' '] *9 for x in range(9)]
+guess_board = [[' '] *9 for x in range(9)]
+
+letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8}
+
+def print_board(board):
+    print('  A B C D E F G H I')
+    print('  -----------------')
+    row_number = 1
+    for row in board:
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_number += 1
+
+def create_ships(board):
+    for ship in range(5):
+        ship_row, ship_column = randint(0,8), randint(0,8)
+        while board[ship_row][ship_column] == 'X':
+            ship_row, ship_column = randint(0,8), randint(0,8)
+        board[ship_row][ship_column] = 'X'
+
+def get_ship_location():
+    row = input('Please Enter A Ship Row 1-9')
+    while row not in '123456789':
+        print('Please Enter A VALID ROW')
+        row = input('Please Enter A Ship Row 1-9')
+    column = input('Please ENter A Letter A To I').upper()
+    while column not in 'ABCDEFGHI':
+        print('Please Enter A VALID Letter')
+        column = input('Please ENter A Letter A To I').upper()
+    return int(row) - 1, lettrs_to_numbers[column]
+
+def count_hit_ship(board):
+    count = 0
+    for row in board:
+        for column in row:
+            if column == 'X':
+                count += 1
+    return count
+
+create_ships(hidden_board)
+turns = 10
+print_board(hidden_board)
+print_board(guess_board)
+
