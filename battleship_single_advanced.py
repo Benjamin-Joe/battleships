6cb16ddfc8f41a94e654d8ca224055cca43ce554
Benjamin-Joe <benjaminjoe43@gmail.com>
2021-09-22 23:53:26 +0000

    Add def place_ships, add check_ship_fits, temporary content

--- battleship_single_advanced.py
+++ battleship_single_advanced.py
@@ -1,5 +1,9 @@
 import random
 
+# orientation
+# Across Vertical
+# length > 9 both ways 
+#  Add random for picking number  
 length_of_ships = [1, 2, 3, 4, 5]
 player_board = [[" "] * 9 for i in range(9)]
 computer_board = [[" "] * 9 for i in range(9)]
@@ -8,13 +12,35 @@ computer_guess_board = [[" "] * 9 for i in range(9)]
 letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8}
 
 def print_board(board):
-    pass
+    print("  A B C D E F G H I")
+    print("  +-+-+-+-+-+-+-+-+")
+    row_number = 1
+    for row in board:
+        print("%d|%s"| % (row_number, "|".join(row)))
+        row_number += 1
 
 def place_ships(board):
-    pass
+    # loop through length of ships
+    for ship_length in length_of_ships:
+        # loop until ship fits on board
+        while True:
+            if board == computer_board:
+                orientation, row,column = random.choice(["Across", "Vertical"]), random.randint(0,8), random.randint(0,8)
+                if check_ship_fits(ship_length, row, column, orientation):
 
-def check_ship_fits():
-    pass
+
+def check_ship_fits(ship_length, row, column, orientation):
+    if orientation == "Across":
+        if column + ship_length > 9:
+            return False
+        else:
+            return True
+    else:
+        if row + ship_length > 9:
+            return False
+        else:
+            return True
+    
 
 def check_overlap():
     pass
