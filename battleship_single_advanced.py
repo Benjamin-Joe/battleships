1288ec2dc1c23d9f163a7f4688f055a7a69b9f97
Benjamin-Joe <benjaminjoe43@gmail.com>
2021-09-22 23:32:59 +0000

    Add length of ships in legend, create empty def for components needed

--- battleship_single_advanced.py
+++ battleship_single_advanced.py
@@ -1,7 +1,32 @@
 import random
 
+length_of_ships = [1, 2, 3, 4, 5]
 player_board = [[" "] * 9 for i in range(9)]
 computer_board = [[" "] * 9 for i in range(9)]
 player_guess_board = [[" "] * 9 for i in range(9)]
 computer_guess_board = [[" "] * 9 for i in range(9)]
 letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8}
+
+def print_board(board):
+    pass
+
+def place_ships(board):
+    pass
+
+def check_ship_fits():
+    pass
+
+def check_overlap():
+    pass
+
+def user_input():
+    pass
+
+def count_hit_ships():
+    pass
+
+def turn(board):
+    pass
+
+while True:
+
