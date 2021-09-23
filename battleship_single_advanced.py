2be6ac2932adb354b6897600f7cd1cfd81edc05e
Benjamin-Joe <benjaminjoe43@gmail.com>
2021-09-23 21:22:26 +0000

    Add place ship ability to game board

--- battleship_single_advanced.py
+++ battleship_single_advanced.py
@@ -1,7 +1,8 @@
 import random
 
 # orientation
-# Across Vertical
+# V = Vertical
+# H = Horizontal
 # length > 9 both ways 
 #  Add random for picking number  
 length_of_ships = [1, 2, 3, 4, 5]
@@ -16,7 +17,7 @@ def print_board(board):
     print("  +-+-+-+-+-+-+-+-+")
     row_number = 1
     for row in board:
-        print("%d|%s"| % (row_number, "|".join(row)))
+        print("%d|%s|" % (row_number, "|".join(row)))
         row_number += 1
 
 def place_ships(board):
@@ -25,12 +26,34 @@ def place_ships(board):
         # loop until ship fits on board
         while True:
             if board == computer_board:
-                orientation, row,column = random.choice(["Across", "Vertical"]), random.randint(0,8), random.randint(0,8)
+                orientation, row, column = random.choice(["H", "V"]), random.randint(0,8), random.randint(0,8)
                 if check_ship_fits(ship_length, row, column, orientation):
+                    if check_overlap(board, row, column, orientation, ship_length)==False:
+                        if orientation == "H":
+                            for i in range(column, column + ship_length):
+                                board [row][i] = "X"
+                        else:
+                            for i in range(row, row + ship_length):
+                                board[i][column] = "X"
+                        break
+            else:
+                place_ships = True
+                print('place the ship with a length of' + str(ship_length))
+                row, column, orientation = user_input(place_ships)
+                if check_ship_fits(ship_length, row, column, orientation):
+                    if check_overlap(board, row, column, orientation, ship_length) == False:
+                        if orientation == "H":
+                            for i in range(column, column + ship_length):
+                                board[row][i] = "X"
+                        else:
+                            for i in range(row, row + ship_length):
+                                board[i][column] = "X"
+                        print_board(player_board)
+                        break
 
 
 def check_ship_fits(ship_length, row, column, orientation):
-    if orientation == "Across":
+    if orientation == "H":
         if column + ship_length > 9:
             return False
         else:
@@ -42,8 +65,16 @@ def check_ship_fits(ship_length, row, column, orientation):
             return True
     
 
-def check_overlap():
-    pass
+def check_overlap(board, row, column, orientation, ship_length):
+    if orientation == "H":
+        for i in range(column, column + ship_length):
+            if board[row][i] == "X":
+                return True
+    else:
+        for i in range(row, row + ship_length):
+            if board[1][column] == "X":
+                return True
+    return False
 
 def user_input():
     pass
@@ -54,5 +85,7 @@ def count_hit_ships():
 def turn(board):
     pass
 
-while True:
 
+print(player_board)
+print(print_board)
+print(computer_board)
